ea669c7aa150dbde5f17de3e90a306d50fc84411
Johan Sellström <johan.sellstrom@gmail.com>
2022-07-25 13:46:02 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ let package = Package(
     name: "DateToolsSwift",
     targets: [
         .target(name: "DateToolsSwift")
-    ],
-    //exclude : ["DateTools", "Examples", "Tests", "DateToolsSwift/Examples"]
+    ]
 )
 //package.exclude = ["DateTools", "Examples", "Tests", "DateToolsSwift/Examples"]
