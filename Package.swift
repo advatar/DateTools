b6c157b5c8bb0acdc13145c0bfe2c4025aab0ca9
Johan Sellström <johan@sellstrom.me>
2022-07-25 13:53:34 +0200

    new format

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 
 let package = Package(
     name: "DateToolsSwift",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v14), .watchOS(.v7)],
+    products: [
+      .library(
+          name: "DateToolsSwift", targets: ["DateToolsSwift"])
+    ],
     targets: [
         .target(name: "DateToolsSwift", path: "DateToolsSwift", exclude: ["Examples", "Tests"])
         
